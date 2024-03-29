d6a309b3d2ce2df22e4cbbd0e4da0c5e3278b8ce
Kyle Michaels <kyle.michaels@gmail.com>
2024-03-29 14:19:12 -0700

    updated mobile layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,17 @@
 const width = window.innerWidth;
 const profile = document.querySelector('.profile')
 const bio = document.querySelector('.bio')
+const projects = document.querySelector('.projects')
+const body = document.querySelector('.body')
 
 if (width <= 1200) {
   profile.classList.add('profileMobile');
   bio.classList.add('bioMobile');
+  projects.classList.add('projectsMobile');
+  body.classList.add('mobile')
 } else {
   profile.classList.remove('profileMobile');
   bio.classList.remove('bioMobile');
+  projects.classList.remove('projectsMobile');
 };
 
